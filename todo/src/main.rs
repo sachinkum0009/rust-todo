4795429138aa4f130c9b5b6418ca8a4a2c15e726
Sachin Kumar <sachinkum123567@gmail.com>
2024-12-20 19:35:00 +0100

    cli updated to add, list and remove tasks

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,39 +1,81 @@
+mod db;
 mod todo;
 
 use clap::Parser;
-// use todo::Todo;
-use todo::Todo;
+use db::db::Db;
+use std::io::{self, Write};
+use prettytable::{Table, row, cell};
+
 
 #[derive(Parser)]
 struct Cli {
     command: String,
-    key: String,
 }
 
-fn main() {
-
-    let something = String::from("hello world");
-    let todo = Todo {
-        name: String::from("Buy groceries"),
-        description: String::from("Buy milk, eggs, and bread"),
-        priority: 1,
-    };
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    println!("Command line: {} {}", args.command, args.key);
+    println!("Command line: {}", args.command);
+
+    let mut task_name = String::new();
+    let mut description = String::new();
+    let mut priority = String::new();
+    let mut index = String::new();
+
+    let db_conn = Db::new(String::from("todos.db")).unwrap();
+    db_conn.create_table()?;
 
     let command = args.command;
     match command.as_str() {
         "add" => {
             println!("Adding a new todo");
+
+            print!("Enter task name: ");
+            io::stdout().flush()?;
+            io::stdin().read_line(&mut task_name)?;
+
+            print!("Enter description: ");
+            io::stdout().flush()?;
+            io::stdin().read_line(&mut description)?;
+
+            print!("Enter priority: ");
+            io::stdout().flush()?;
+            io::stdin().read_line(&mut priority)?;
+
+            let priority: i32 = priority.trim().parse()?;
+            println!("Task name: {}, Description: {}, Priority: {}", task_name.trim(), description.trim(), priority);
+
+            db_conn.add_todo(&task_name.trim(), &description.trim(), priority)?;
+            println!("Todo added successfully");
         },
         "remove" => {
             println!("Removing a todo");
+            print!("Enter the index of the todo to remove: ");
+            io::stdout().flush()?;
+            io::stdin().read_line(&mut index)?;
+
+            let index_: i32 = index.trim().parse()?;
+            db_conn.remove_todo(index_)?;
+
         },
         "list" => {
             println!("Listing all todos");
+            let todos = db_conn.get_todos()?;
+            println!("List of Todos");
+            let mut table = Table::new();
+            table.add_row(row!["ID", "Task Name", "Description", "Priority"]);
+
+            for todo in todos {
+                table.add_row(row![todo.id, todo.name, todo.description, todo.priority]);
+            }
+            table.printstd();
+        },
+        "help" => {
+            println!("Help");
         },
         _ => {
             println!("Unknown command");
         }
     }
+
+    Ok(())
 }
--- todo/src/todo.rs
+++ todo/src/todo.rs
@@ -1,5 +1,23 @@
+#[derive(Debug)]
 pub struct Todo {
+    pub id: i32,
     pub name: String,
     pub description: String,
     pub priority: i32,
+}
+
+impl Todo {
+    pub fn new(name: String, description: String, priority: i32) -> Todo {
+        Todo {
+            id: 0,
+            name,
+            description,
+            priority,
+        }
+    }
+    pub fn print_details(&self) {
+        println!("Name: {}", self.name);
+        println!("Description: {}", self.description);
+        println!("Priority: {}", self.priority);
+    }
 }
